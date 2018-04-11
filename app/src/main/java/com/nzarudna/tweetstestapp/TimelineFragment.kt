6491559cb24ed3f492fe93bfb7e6f139672ea9c9
nataliia-zarudna <natalija.sirobaba@gmail.com>
2018-04-12 01:01:51 +0300

    Auth requests

--- app/src/main/java/com/nzarudna/tweetstestapp/TimelineFragment.kt
+++ app/src/main/java/com/nzarudna/tweetstestapp/TimelineFragment.kt
@@ -3,10 +3,16 @@ package com.nzarudna.tweetstestapp
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.android.volley.Response
 import com.nzarudna.tweetstestapp.databinding.FragmentTimelineBinding
+import kotlinx.android.synthetic.main.fragment_timeline.*
 
 /**
  * Created by Nataliia on 11.04.2018.
@@ -22,6 +28,42 @@ class TimelineFragment : Fragment() {
 
         mViewModel = TimelineViewModel()
         (activity.application as TweetsTestApplication).appComponent.inject(mViewModel)
+
+        val webView = login_view
+
+        //FirebaseApp firebaseApp = FirebaseApp.initializeApp(this);
+        //mAuth = FirebaseAuth.getInstance();
+
+        TwitterAuthManager.getRequestToken(this, Response.Listener<String> { response ->
+            //Log.d(TAG, "response " + response)
+
+            val oauthToken = TwitterAuthManager.getResponseParamValue(response, "oauth_token")
+
+            val webViewClient = object : WebViewClient() {
+
+                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
+
+                override fun onPageFinished(view: WebView, url: String) {
+
+                    if (url.startsWith(BuildConfig.CALLBACK_URL)) {
+                        val urlParams = url.split("\\?".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+                        val authVerifier = TwitterAuthManager.getResponseParamValue(urlParams, "oauth_verifier")
+
+                        TwitterAuthManager.getRequestToken(this@MainActivity, Response.Listener<String> { response ->
+                            Log.d(TAG, response)
+
+                            val authVerifier = TwitterAuthManager.getResponseParamValue(urlParams, "oauth_verifier")
+                        }, oauthToken, authVerifier)
+                    }
+
+                    super.onPageFinished(view, url)
+                }
+            }
+            webView.setWebViewClient(webViewClient)
+            webView.loadUrl("https://api.twitter.com/oauth/authenticate?oauth_token=" + oauthToken)
+        }, null, null)
 
         return fragmentView.root
     }
--- app/src/main/java/com/nzarudna/tweetstestapp/TwitterAuthManager.kt
+++ app/src/main/java/com/nzarudna/tweetstestapp/TwitterAuthManager.kt
@@ -1,5 +1,21 @@
 package com.nzarudna.tweetstestapp
 
+import android.content.Context
+import android.util.Base64
+import android.util.Log
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 import javax.inject.Singleton
 
 /**
@@ -7,4 +23,154 @@ import javax.inject.Singleton
  */
 @Singleton
 class TwitterAuthManager {
+
+    private val TAG = "TwitterAuthManager"
+
+    private val AUTH_REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+    private val AUTH_ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+    fun getRequestToken(context: Context, listener: Response.Listener<String>, oauthToken: String?, oauthVerifier: String?) {
+
+        val url = if (oauthToken == null) AUTH_REQUEST_TOKEN_URL else AUTH_ACCESS_TOKEN_URL
+
+        val request = object : StringRequest(Request.Method.POST, url,
+                listener,
+                object : Response.ErrorListener {
+                    override fun onErrorResponse(error: VolleyError) {
+                        Log.e(TAG, "onErrorResponse " + String(error.networkResponse.data, StandardCharsets.UTF_8), error)
+                    }
+                }) {
+            override fun getHeaders(): MutableMap<String, String> {
+
+                val oauthHeaders = TreeMap<String, String>()
+
+                oauthHeaders.put("oauth_consumer_key", BuildConfig.CONSUMER_KEY)
+                oauthHeaders.put("oauth_signature_method", "HMAC-SHA1")
+                oauthHeaders.put("oauth_timestamp", URLEncoder.encode((Date().time / 1000).toString()))
+
+                val nonce = String(Base64.encode(Math.random().toString().toByteArray(), Base64.DEFAULT))
+                oauthHeaders.put("oauth_nonce", nonce.trim { it <= ' ' })
+
+                oauthHeaders.put("oauth_version", "1.0")
+
+                if (oauthToken != null) {
+                    oauthHeaders.put("oauth_token", oauthToken)
+                } else {
+                    oauthHeaders.put("oauth_callback", BuildConfig.CALLBACK_URL)
+                }
+
+                var signature = ""
+                try {
+                    signature = getSignature("POST", url, oauthHeaders, params, BuildConfig.CONSUMER_SECRET_KEY, null)
+
+                } catch (e: NoSuchAlgorithmException) {
+                    e.printStackTrace()
+                } catch (e: InvalidKeyException) {
+                    e.printStackTrace()
+                }
+
+                oauthHeaders.put("oauth_signature", signature.trim { it <= ' ' })
+
+                val encodedHeaders = HashMap<String, String>()
+                for (header in oauthHeaders.keys) {
+                    val paramValue = oauthHeaders[header]
+                    encodedHeaders.put(header, URLEncoder.encode(paramValue))
+                }
+
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "OAuth " + glueParams(encodedHeaders))
+
+                return headers
+            }
+
+            override fun getParams(): MutableMap<String, String> {
+
+                val params = HashMap<String, String>()
+                if (oauthVerifier != null) {
+                    params.put("oauth_verifier", oauthVerifier)
+                }
+                return params
+            }
+        }
+
+        val requestQueue = Volley.newRequestQueue(context)
+        requestQueue.add(request)
+    }
+
+    @Throws(NoSuchAlgorithmException::class, InvalidKeyException::class)
+    private fun getSignature(method: String, baseURL: String, headers: Map<String, String>, params: Map<String, String>, consumerSecret: String, oauthSecret: String?): String {
+        var oauthSecret = oauthSecret
+
+        val allParams = TreeMap(headers)
+        allParams.putAll(params)
+
+        val signatureBaseString = StringBuilder()
+
+        signatureBaseString
+                .append(method.toUpperCase())
+                .append("&")
+                .append(URLEncoder.encode(baseURL))
+                .append('&')
+
+        val paramsSignaturePart = StringBuilder()
+        for (key in allParams.keys) {
+            paramsSignaturePart
+                    .append(key)
+                    .append('=')
+
+            val value = if ("oauth_callback" == key) URLEncoder.encode(allParams[key]) else allParams[key]
+            paramsSignaturePart.append(value)
+                    .append('&')
+        }
+        paramsSignaturePart.deleteCharAt(paramsSignaturePart.length - 1)
+
+        val encodedParamsSignaturePart = URLEncoder.encode(paramsSignaturePart.toString())
+        signatureBaseString.append(encodedParamsSignaturePart)
+
+        val mac = Mac.getInstance("HMAC-SHA1")
+
+        if (oauthSecret == null) {
+            oauthSecret = ""
+        }
+        val signingKey = consumerSecret + "&" + oauthSecret
+
+        val secretKeySpec = SecretKeySpec(signingKey.toByteArray(), mac.algorithm)
+        mac.init(secretKeySpec)
+
+        val digest = mac.doFinal(signatureBaseString.toString().toByteArray())
+        val signature = Base64.encode(digest, Base64.DEFAULT)
+
+        return String(signature)
+    }
+
+    private fun glueParams(params: Map<String, String>): String {
+        val builder = StringBuilder()
+
+        for (key in params.keys) {
+            builder
+                    .append(key)
+                    .append('=')
+                    .append('"')
+                    .append(params[key])
+                    .append('"')
+                    .append(',')
+        }
+        builder.deleteCharAt(builder.length - 1)
+
+        return builder.toString()
+    }
+
+    fun getResponseParamValue(response: String, param: String): String? {
+
+        val params = response.split("\\&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        for (i in params.indices) {
+            val paramTokens = params[i].split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            if (param == paramTokens[0]) {
+                return paramTokens[1]
+            }
+        }
+        return null
+    }
+
+
 }
